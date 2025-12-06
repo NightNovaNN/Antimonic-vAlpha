e0455762354780c909d0e59d1a75dffb13b18f86
ISDNightNova <nightnovann@gmail.com>
2025-12-06 19:27:39 +0530

    Update DLL path and refactor rendering loop

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from time import sleep
 import time 
 
 # Load the Antimonic renderer DLL
-ar = ctypes.CDLL("./antimonic.dll")
+ar = ctypes.CDLL(r"C:\Users\Admin\OneDrive\Desktop\Trappist\Antimonic\antimonic.dll ")
 
 # Function signatures
 ar.ar_init.argtypes = [ctypes.c_int, ctypes.c_int, ctypes.c_char_p]
@@ -11,13 +11,17 @@ ar.ar_clear.argtypes = [ctypes.c_uint32]
 ar.ar_draw_rect.argtypes = [ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_uint32]
 ar.ar_present.argtypes = []
 
+# =================================
+# Test rendering
+# =================================
+
 # Init window
 ar.ar_init(800, 600, b"Antimonic Renderer Python Test")
 
-start = time.perf_counter_ns()
-ar.ar_clear(0x202020)        # Dark gray
-ar.ar_draw_rect(100, 100, 64, 64, 0xFF0000)  # Red square
-ar.ar_present()
-end = time.perf_counter_ns()
-print(f"Frame rendered in {(end - start) / 1_000_000} ms")
-sleep(0.016)                 # ~60 FPS
+
+while True:
+
+    ar.ar_clear(0x202020)        # Dark gray
+    ar.ar_draw_rect(100, 100, 64, 64, 0xFF0000)  # Red square
+    ar.ar_present()
+    sleep(0.016)                 # ~60 FPS
